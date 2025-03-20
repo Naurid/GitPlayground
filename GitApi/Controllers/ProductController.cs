44008b4a5792f29e71b6c2a670bde4484fbf41ed
Gabor Vilics <vilics@hotmail.com>
2025-03-20 09:24:21 +0100

    formatted Controller

--- GitApi/Controllers/ProductController.cs
+++ GitApi/Controllers/ProductController.cs
@@ -6,26 +6,30 @@ namespace GitApi.Controllers;
 [Route("api/[controller]")]
 public class ProductController(IList<Product> pl): ControllerBase
 {
-   [HttpPost("newProduct")]
-   public IActionResult CreateProduct([FromBody] Product newproduct){
+    [HttpPost("newProduct")]
+    public IActionResult CreateProduct([FromBody] Product newproduct)
+    {
         pl.Add(newproduct);
         return Ok();
-   }
+    }
 
     [HttpGet("ProductById/{id}")]
-   public IActionResult GetProduct(int id){
+    public IActionResult GetProduct(int id)
+    {
         return Ok(pl.FirstOrDefault(x => x.Id == id));
-   }
+    }
 
     [HttpGet("ProductByName/{name}")]
-   public IActionResult GetProduct(string name){
+    public IActionResult GetProduct(string name)
+    {
         return Ok(pl.FirstOrDefault(x => x.Name == name));
-   }
+    }
 
-     [HttpGet("Products")]
-   public IActionResult GetProducts(){
+    [HttpGet("Products")]
+    public IActionResult GetProducts()
+    {
         return Ok(pl);
-   }
+    }
 
     [HttpPut("ProductName/{id}/{name}")]
     public IActionResult ChangeProductName(int id, string name)
